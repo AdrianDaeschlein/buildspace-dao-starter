eface101a44d82f55af558bcc80ca948872514ae
Adrian <adrian.daeschlein@online.de>
2022-05-17 16:59:39 +0200

    ADD almost final steps

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,23 @@
+import sdk from "./1-initialize-sdk.js";
+
+const token = sdk.getToken("0x3D425f63A3dF6718b9129ABd1fbd6516852f48F5");
+
+(async () => {
+  try {
+    // Log the current roles.
+    const allRoles = await token.roles.getAll();
+
+    console.log("👀 Roles that exist right now:", allRoles);
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await token.roles.setAll({ admin: [], minter: [] });
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await token.roles.getAll()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO trasury", error);
+  }
+})();
